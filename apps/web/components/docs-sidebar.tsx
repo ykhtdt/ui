8f328f9279d8050fe80a3eaa9b992274f5bace04
jgpark-1 <jgpark7281@gmail.com>
2025-07-28 15:41:53 +0900

    feat: remove icon, border, background

--- apps/web/components/docs-sidebar.tsx
+++ apps/web/components/docs-sidebar.tsx
@@ -1,10 +1,4 @@
-import {
-  Calendar,
-  Home,
-  Inbox,
-  Search,
-  Settings,
-} from "lucide-react"
+import Link from "next/link"
 
 import {
   Sidebar,
@@ -21,120 +15,103 @@ const items = [
   {
     title: "Home",
     url: "#",
-    icon: Home,
   },
   {
     title: "Inbox",
     url: "#",
-    icon: Inbox,
   },
   {
     title: "Calendar",
     url: "#",
-    icon: Calendar,
   },
   {
     title: "Search",
     url: "#",
-    icon: Search,
   },
   {
     title: "Settings",
     url: "#",
-    icon: Settings,
   },
   {
     title: "Settings1",
     url: "#",
-    icon: Settings,
   },
   {
     title: "Settings2",
     url: "#",
-    icon: Settings,
   },
   {
     title: "Settings3",
     url: "#",
-    icon: Settings,
   },
   {
     title: "Settings4",
     url: "#",
-    icon: Settings,
   },
   {
     title: "Settings5",
     url: "#",
-    icon: Settings,
   },
   {
     title: "Settings6",
     url: "#",
-    icon: Settings,
   },
   {
     title: "Settings7",
     url: "#",
-    icon: Settings,
   },
   {
     title: "Settings8",
     url: "#",
-    icon: Settings,
   },
   {
     title: "Settings9",
     url: "#",
-    icon: Settings,
   },
   {
     title: "Settings99",
     url: "#",
-    icon: Settings,
   },
   {
     title: "Settings98",
     url: "#",
-    icon: Settings,
   },
   {
     title: "Settings97",
     url: "#",
-    icon: Settings,
   },
   {
     title: "Settings96",
     url: "#",
-    icon: Settings,
   },
   {
     title: "Settings95",
     url: "#",
-    icon: Settings,
   },
   {
     title: "Settings94",
     url: "#",
-    icon: Settings,
   },
 ]
 
 export function DocsSidebar() {
   return (
-    <Sidebar className="sticky top-[calc(var(--header-height))] h-[calc(100svh-var(--header-height)-var(--footer-height))]">
-      <SidebarContent className="no-scrollbar">
+    <Sidebar className="sticky top-[calc(var(--header-height))] h-[calc(100svh-var(--header-height)-var(--footer-height))] border-none">
+      <SidebarContent className="no-scrollbar bg-background">
         <SidebarGroup>
-          <SidebarGroupLabel>Application</SidebarGroupLabel>
+          <SidebarGroupLabel>
+            Application
+          </SidebarGroupLabel>
           <SidebarGroupContent>
             <SidebarMenu>
               {items.map((item) => (
                 <SidebarMenuItem key={item.title}>
                   <SidebarMenuButton asChild>
-                    <a href={item.url}>
-                      <item.icon />
-                      <span>{item.title}</span>
-                    </a>
+                    <Link href={item.url}>
+                      <span>
+                        {item.title}
+                      </span>
+                    </Link>
                   </SidebarMenuButton>
                 </SidebarMenuItem>
               ))}
