c9f96f9b0f0012554d63a96b37be1afa9787366d
jgpark-1 <jgpark7281@gmail.com>
2025-07-28 15:39:55 +0900

    refactor: move page component to top of file

--- apps/web/app/docs/components/[slug]/page.tsx
+++ apps/web/app/docs/components/[slug]/page.tsx
@@ -10,6 +10,24 @@ import { MDXComponents } from "@/components/mdx-components"
 
 const PATH = "content\\components\\"
 
+export default async function Page({
+  params
+}: {
+  params: Promise<{ slug: string }>
+}) {
+  const { slug } = await params
+
+  const bundledMDX = await getBundleMDX("components", slug)
+
+  if (!bundledMDX) {
+    return notFound()
+  }
+
+  return (
+    <MDXComponents code={bundledMDX.code} />
+  )
+}
+
 export async function generateMetadata({
   params
 }: {
@@ -28,23 +46,6 @@ export async function generateMetadata({
   }
 }
 
-export default async function Page({
-  params
-}: {
-  params: Promise<{ slug: string }>
-}) {
-  const { slug } = await params
-
-  const bundledMDX = await getBundleMDX("components", slug)
-
-  if (!bundledMDX) {
-    return notFound()
-  }
-
-  return (
-    <MDXComponents code={bundledMDX.code} />
-  )
-}
 
 export const generateStaticParams = async () => {
   const frontmatters = getAllFrontmatter("components")
