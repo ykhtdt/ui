e214c61bd10ee4b000ec47c2a16693b2bc01792e
Jonggwang Park <jgpark7281@gmail.com>
2025-07-31 18:42:29 +0900

    feat: mode toggle to button with click handler

--- apps/web/components/mode-toggle.tsx
+++ apps/web/components/mode-toggle.tsx
@@ -1,40 +1,32 @@
 "use client"
 
-import * as React from "react"
-import { Moon, Sun } from "lucide-react"
 import { useTheme } from "next-themes"
 
-import { Button } from "@workspace/ui/components/button"
 import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@workspace/ui/components/dropdown-menu"
+  MoonIcon,
+  SunIcon,
+} from "lucide-react"
 
-export function ModeToggle() {
-  const { setTheme } = useTheme()
+import { Button } from "@workspace/ui/components/button"
+
+export const ModeToggle = () => {
+  const { theme, systemTheme, setTheme } = useTheme()
+
+  const handleClick = () => {
+    if (systemTheme === "light" || theme === "light") {
+      setTheme("dark")
+    } else if (systemTheme === "dark" || theme === "dark") {
+      setTheme("light")
+    }
+  }
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon">
-          <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
-          <span className="sr-only">Toggle theme</span>
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
-        <DropdownMenuItem onClick={() => setTheme("light")}>
-          Light
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("dark")}>
-          Dark
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("system")}>
-          System
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <Button variant="outline" size="icon" onClick={handleClick} className="cursor-pointer size-5 border-none bg-inherit p-0 text-foreground shadow-none hover:bg-inherit focus:outline-none focus-visible:ring-0">
+      <SunIcon className="size-4 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+      <MoonIcon className="absolute size-4 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+      <span className="sr-only">
+        Toggle theme
+      </span>
+    </Button>
   )
 }
