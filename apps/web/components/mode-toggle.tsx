4295aacdc986f5bb5cf3f2953fadb7d7d819435d
jgpark-1 <jgpark7281@gmail.com>
2025-08-04 23:51:07 +0900

    feat: change button variant from outline to ghost

--- apps/web/components/mode-toggle.tsx
+++ apps/web/components/mode-toggle.tsx
@@ -21,7 +21,7 @@ export const ModeToggle = () => {
   }
 
   return (
-    <Button variant="outline" size="icon" onClick={handleClick} className="cursor-pointer size-5 border-none bg-inherit p-0 text-foreground shadow-none hover:bg-inherit focus:outline-none focus-visible:ring-0">
+    <Button variant="ghost" size="icon" onClick={handleClick} className="cursor-pointer size-5 border-none bg-inherit p-0 text-foreground shadow-none hover:bg-inherit focus:outline-none focus-visible:ring-0">
       <SunIcon className="size-4 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
       <MoonIcon className="absolute size-4 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
       <span className="sr-only">
