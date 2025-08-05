975ccc7c2da35ddf2f58423b755df44c3c232a14
Jonggwang Park <jgpark7281@gmail.com>
2025-08-05 15:08:45 +0900

    feat: add NavigationDrawer example in docs layout

--- apps/web/app/docs/layout.tsx
+++ apps/web/app/docs/layout.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link"
+
 import { ArrowUpIcon } from "lucide-react"
 
 import { SidebarProvider } from "@workspace/ui/components/sidebar"
@@ -6,6 +8,104 @@ import { ModeToggle } from "@/components/mode-toggle"
 import { DocsSidebar } from "@/components/docs-sidebar"
 import { DocsToc } from "@/components/docs-toc"
 import { ScrollToTopButton } from "@/components/scroll-top"
+
+import {
+  NavigationDrawer,
+  NavigationDrawerTrigger,
+  NavigationDrawerContent,
+  NavigationDrawerHeader,
+  NavigationDrawerTitle,
+  NavigationDrawerDescription,
+  NavigationDrawerGroup,
+  NavigationDrawerGroupLabel,
+  NavigationDrawerGroupContent,
+  NavigationDrawerMenu,
+  NavigationDrawerMenuItem,
+  NavigationDrawerMenuButton,
+  NavigationDrawerFooter,
+} from "@/ui/navigation-drawer"
+
+const items = [
+  {
+    title: "Components",
+    url: "/docs/components",
+    children: [
+      {
+        title: "Table of Contents",
+        url: "/docs/components/toc",
+      },
+      {
+        title: "Navigation Drawer",
+        url: "/docs/components/navigation-drawer",
+      },
+    ],
+  },
+  {
+    title: "Layouts",
+    url: "/docs/layouts",
+    children: [],
+  },
+  {
+    title: "Layouts1",
+    url: "/docs/layouts1",
+    children: [],
+  },
+  {
+    title: "Layouts2",
+    url: "/docs/layouts2",
+    children: [],
+  },
+  {
+    title: "Layouts3",
+    url: "/docs/layouts3",
+    children: [],
+  },
+  {
+    title: "Layouts4",
+    url: "/docs/layouts4",
+    children: [],
+  },
+  {
+    title: "Layouts5",
+    url: "/docs/layouts5",
+    children: [],
+  },
+  {
+    title: "Layouts6",
+    url: "/docs/layouts6",
+    children: [],
+  },
+  {
+    title: "Layouts7",
+    url: "/docs/layouts7",
+    children: [],
+  },
+  {
+    title: "Layouts8",
+    url: "/docs/layouts8",
+    children: [],
+  },
+  {
+    title: "Layouts9",
+    url: "/docs/layouts9",
+    children: [],
+  },
+  {
+    title: "Layouts10",
+    url: "/docs/layouts10",
+    children: [],
+  },
+  {
+    title: "Layouts11",
+    url: "/docs/layouts11",
+    children: [],
+  },
+  {
+    title: "Layouts12",
+    url: "/docs/layouts12",
+    children: [],
+  },
+]
 
 export default function Layout({
   children,
@@ -28,6 +128,40 @@ export default function Layout({
         <header className="sticky top-0 z-50 w-full bg-background">
           <div className="w-full max-w-9xl mx-auto px-4 sm:px-2">
             <div className="h-(--header-height) flex items-center gap-2">
+              <NavigationDrawer>
+                <NavigationDrawerTrigger />
+                <NavigationDrawerContent>
+                  <NavigationDrawerHeader>
+                    <NavigationDrawerTitle>eXpandable UI</NavigationDrawerTitle>
+                    <NavigationDrawerDescription />
+                  </NavigationDrawerHeader>
+                  {items.map((item) => (
+                    <NavigationDrawerGroup key={item.title}>
+                      <NavigationDrawerGroupLabel>
+                        {item.title}
+                      </NavigationDrawerGroupLabel>
+                      <NavigationDrawerGroupContent>
+                        <NavigationDrawerMenu>
+                          {item.children.map((child) => (
+                            <NavigationDrawerMenuItem key={child.title}>
+                              <NavigationDrawerMenuButton asChild>
+                                <Link href={child.url}>
+                                  <span>
+                                    {child.title}
+                                  </span>
+                                </Link>
+                              </NavigationDrawerMenuButton>
+                            </NavigationDrawerMenuItem>
+                          ))}
+                        </NavigationDrawerMenu>
+                      </NavigationDrawerGroupContent>
+                    </NavigationDrawerGroup>
+                  ))}
+                  <NavigationDrawerFooter className="border-t">
+                    {/* <ModeToggle /> */}
+                  </NavigationDrawerFooter>
+                </NavigationDrawerContent>
+              </NavigationDrawer>
               <ModeToggle />
             </div>
           </div>
